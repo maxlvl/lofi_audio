f9ab3e14a3a7d1c6fad0ce6aaa8ab2bbd750b2e3
maxlvl <max@maxvl.com>
2021-02-02 19:53:17 -0500

    only play on document loaded

--- script.js
+++ script.js
@@ -9,98 +9,11 @@ modalButton.addEventListener('click', openModal);
 
 modalOuter.addEventListener('click', checkIfCloseModal)
 
-window.addEventListener('keyup', playSoundFromKey)
-
 const playingState = {
   masterBeat: {
     isPlaying: false
   }
 };
-
-keys.forEach(key => key.addEventListener('click', playSound))
-keys.forEach(key => {
-    let keyNumber = key.dataset.key
-    playingState[keyNumber] = {isPlaying: false} }
-)
-
-function startBeat() {
-  let audio = document.querySelectorAll('audio');
-
-  audio.forEach(audio => {
-    audio.volume = 0
-    audio.loop = true
-    audio.play()
-  })
-
-  playingState.masterBeat.isPlaying = true
-}
-
-
-function playSound(event) {
-  let keyEl = event.currentTarget
-  let key = event.currentTarget.dataset.key
-  let keyAudio = document.querySelector(`audio[data-key="${key}"]`)
-
-  if (!playingState.masterBeat.isPlaying) {
-    startBeat()
-  }
-
-  keyAudio.volume = 1
-
-  keyEl.classList.add('playing')
-
-  keyEl.addEventListener('click', stopSound)
-  keyEl.removeEventListener('click', playSound)
-    
-}
-
-function stopSound(event) {
-  let keyEl = event.currentTarget
-  let key = event.currentTarget.dataset.key
-  let audio = document.querySelector(`audio[data-key="${key}"]`)
-
-  audio.volume = 0
-
-  keyEl.classList.remove('playing')
-
-  keyEl.addEventListener('click', playSound)
-  keyEl.removeEventListener('click', stopSound)
-}
-
-
-function playSoundFromKey(event) {
-  let key = event.key;
-  if (!['1', '2', '3', '4'].includes(key)) {
-    return
-  }
-  let keyEl = document.querySelector(`div[data-key="${key}"]`) 
-  let keyAudio = document.querySelector(`audio[data-key="${key}"]`)
-  
-  if (!playingState.masterBeat.isPlaying) {
-    startBeat()
-  }
-  keyEl.classList.add('playing')
-  keyAudio.volume = 1
-
-  window.addEventListener('keyup', stopSoundFromKey)
-  window.removeEventListener('keyup', playSoundFromKey)
-}
-
-function stopSoundFromKey(event) {
-  let key = event.key;
-  if (!['1', '2', '3', '4'].includes(key)) {
-    return
-  }
-  let keyEl = document.querySelector(`div[data-key="${key}"]`) 
-  let audio = document.querySelector(`audio[data-key="${key}"]`)
-
-  audio.volume = 0
-
-  keyEl.classList.remove('playing')
-  window.addEventListener('keyup', playSoundFromKey)
-  window.removeEventListener('keyup', stopSoundFromKey)
-
-}
 
 function checkIfCloseModal(event) {
   const outsideClicked = !event.target.closest('.creditsmodal-inner');
@@ -117,3 +30,93 @@ function closeModal() {
   modalOuter.classList.remove('open')
   container.classList.remove('is-blurred')
 }
+
+window.addEventListener("load", event => {
+  keys.forEach(key => key.addEventListener('click', playSound))
+  keys.forEach(key => {
+      let keyNumber = key.dataset.key
+      playingState[keyNumber] = {isPlaying: false} }
+  )
+
+  window.addEventListener('keyup', playSoundFromKey)
+  
+  function startBeat() {
+    let audio = document.querySelectorAll('audio');
+  
+    audio.forEach(audio => {
+      audio.volume = 0
+      audio.loop = true
+      audio.play()
+    })
+  
+    playingState.masterBeat.isPlaying = true
+  }
+  
+  
+  function playSound(event) {
+    let keyEl = event.currentTarget
+    let key = event.currentTarget.dataset.key
+    let keyAudio = document.querySelector(`audio[data-key="${key}"]`)
+  
+    if (!playingState.masterBeat.isPlaying) {
+      startBeat()
+    }
+  
+    keyAudio.volume = 1
+  
+    keyEl.classList.add('playing')
+  
+    keyEl.addEventListener('click', stopSound)
+    keyEl.removeEventListener('click', playSound)
+      
+  }
+  
+  function stopSound(event) {
+    let keyEl = event.currentTarget
+    let key = event.currentTarget.dataset.key
+    let audio = document.querySelector(`audio[data-key="${key}"]`)
+  
+    audio.volume = 0
+  
+    keyEl.classList.remove('playing')
+  
+    keyEl.addEventListener('click', playSound)
+    keyEl.removeEventListener('click', stopSound)
+  }
+  
+  
+  function playSoundFromKey(event) {
+    let key = event.key;
+    if (!['1', '2', '3', '4'].includes(key)) {
+      return
+    }
+    let keyEl = document.querySelector(`div[data-key="${key}"]`) 
+    let keyAudio = document.querySelector(`audio[data-key="${key}"]`)
+    
+    if (!playingState.masterBeat.isPlaying) {
+      startBeat()
+    }
+    keyEl.classList.add('playing')
+    keyAudio.volume = 1
+  
+    window.addEventListener('keyup', stopSoundFromKey)
+    window.removeEventListener('keyup', playSoundFromKey)
+  }
+  
+  function stopSoundFromKey(event) {
+    let key = event.key;
+    if (!['1', '2', '3', '4'].includes(key)) {
+      return
+    }
+    let keyEl = document.querySelector(`div[data-key="${key}"]`) 
+    let audio = document.querySelector(`audio[data-key="${key}"]`)
+  
+    audio.volume = 0
+  
+    keyEl.classList.remove('playing')
+    window.addEventListener('keyup', playSoundFromKey)
+    window.removeEventListener('keyup', stopSoundFromKey)
+  
+  }
+}) 
+
