587cb581235bf8e6e85d7adaba0d866a684d5d46
maxlvl <max@maxvl.com>
2021-02-02 21:11:55 -0500

    Convert wav to ogg

--- script.js
+++ script.js
@@ -15,6 +15,28 @@ const playingState = {
   }
 };
 
+
+
+// fetch("https://s3-us-west-2.amazonaws.com/s.cdpn.io/254249/break.ogg")
+//   .then(response => response.arrayBuffer())
+//   .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
+//   .then(audioBuffer => {
+//     buffer = audioBuffer;
+//   });
+
+// function start() {
+//   source = context.createBufferSource();
+//   source.buffer = buffer;
+//   source.connect(context.destination);
+//   source.loop = true;
+//   source.start();
+// }
+
+// function stop() {
+//   source.stop();
+// }
+
+
 function checkIfCloseModal(event) {
   const outsideClicked = !event.target.closest('.creditsmodal-inner');
 
